963f16f3cec892690ac6a42cccd0df255f7c4fe6
jey1987 <ms.jramalingam@users.noreply.github.com>
2019-10-19 12:36:58 -0400

    Create Project_3.sql

---
+++ Project3/Project_3.sql
@@ -0,0 +1,15 @@
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+SET GLOBAL local_infile = 1;
+DROP TABLE job_listings ;
+CREATE TABLE job_listings 
+(
+X1 varchar(1000),
+title varchar(200),
+company varchar(100),
+location varchar(100),
+summary varchar(1000),
+link text,
+description text
+);
+
+select count(*) from job_listings ;
