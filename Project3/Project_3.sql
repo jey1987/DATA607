28e50c1203b136ed1142ee973afc41595c6cdc94
jey1987 <ms.jramalingam@users.noreply.github.com>
2019-10-29 22:01:24 -0400

    Updated

--- Project3/Project_3.sql
+++ Project3/Project_3.sql
@@ -1,6 +1,9 @@
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+
 SET GLOBAL local_infile = 1;
+
 DROP TABLE job_listings ;
+
 CREATE TABLE job_listings 
 (
 X1 varchar(1000),
